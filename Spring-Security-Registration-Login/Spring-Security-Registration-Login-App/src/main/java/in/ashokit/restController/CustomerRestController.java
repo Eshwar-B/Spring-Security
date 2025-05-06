c258aae030f8dc24a5c2d7d556192f072e58c0aa
Eshwar-B <1728eshwar@gmail.com>
2025-05-06 18:52:26 +0530

    Registration and Login REST APIs Implemented

---
+++ Spring-Security-Registration-Login/Spring-Security-Registration-Login-App/src/main/java/in/ashokit/restController/CustomerRestController.java
@@ -0,0 +1,62 @@
+package in.ashokit.restController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.ashokit.entity.Customer;
+import in.ashokit.repo.CustomerRepository;
+
+@RestController
+public class CustomerRestController {
+
+	@Autowired
+	private CustomerRepository customerRepo;
+
+	@Autowired
+	private PasswordEncoder pwdEncoder;
+
+	@Autowired
+	private AuthenticationManager authManager;
+
+	@PostMapping("/register")
+	public ResponseEntity<String> saveCustomer(@RequestBody Customer customer) {
+		String password = pwdEncoder.encode(customer.getPwd());
+
+		customer.setPwd(password);
+
+		customerRepo.save(customer);
+
+		return new ResponseEntity<>("Customer Registered", HttpStatus.CREATED);
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<String> loginCustomer(@RequestBody Customer customer) {
+
+		UsernamePasswordAuthenticationToken emailPwdAuthToken = 
+								new UsernamePasswordAuthenticationToken(customer.getEmail(), customer.getPwd());
+		
+		try {
+			
+			Authentication authenticate = authManager.authenticate(emailPwdAuthToken);
+			
+			if(authenticate.isAuthenticated()) {
+				return new ResponseEntity<>("Welcome to the Website!! , You are a Trusted User", HttpStatus.OK);
+			}
+			
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		return new ResponseEntity<>("Invalid Credentials", HttpStatus.BAD_REQUEST);
+	}
+
+}
